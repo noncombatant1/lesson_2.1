56219c0866771d1df5e452510c465278b6145681
noncombatant1 <dubovetsaleksey@gmail.com>
2021-11-02 17:18:44 +0600

    created

--- trening/trening/Program.cs
+++ trening/trening/Program.cs
@@ -1,33 +1,57 @@
 ﻿using System;
 
-namespace trening
+namespace ConsoleApplication2
 {
     class Program
     {
+        const int TABLEWIDTH = 77;
+
         static void Main(string[] args)
         {
-            int n = Convert.ToInt32(Console.ReadLine());
-            int maxspeed = 0;
-            int number;
-            string minspeed;
-            for (int i = 0; i < n; i++)
+            PrintLine();
+            PrintRow(new string[] { "Field", "Type", "Null" });
+            PrintLine();
+            PrintRow(new string[] { "id", "int(11)", "NO" });
+            PrintRow(new string[] { "name", "varchar(25)", "NO" });
+            PrintRow(new string[] { "age", "int(3)", "NO" });
+            PrintLine();
+
+            Console.ReadKey();
+        }
+
+        static void PrintLine()
+        {
+            Console.WriteLine(new string('-', TABLEWIDTH));
+        }
+
+        static void PrintRow(params string[] columns)
+        {
+            int width = (TABLEWIDTH - columns.Length) / columns.Length;
+            string row = "|";
+
+            foreach (string column in columns)
             {
-                number = Convert.ToInt32(Console.ReadLine());
-                if (number > maxspeed)
-                {
-                    maxspeed = number;
-                }
-                
-                if (number < 30)
-                {
-                    minspeed = "YES";
-                }
-                else
-                {
-                    minspeed = "NO";
-                }
+                row += AlignCentre(column, width) + "|";
             }
-            Console.Write(maxspeed + " " + minspeed);
+
+            Console.WriteLine(row);
+        }
+
+        static string AlignCentre(string text, int width)
+        {
+            text = text.Length > width ? text.Substring(0, width - 3) + "..." : text;
+
+            if (string.IsNullOrEmpty(text))
+            {
+                return new string(' ', width);
+            }
+            else
+            {
+                return text.PadRight(width - (width - text.Length) / 2).PadLeft(width);
+            }
         }
     }
 }
+
+
+
